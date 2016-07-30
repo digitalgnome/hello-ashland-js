919ec737582fb2477e783c33cbe9df7d431f3c68
Tariq Khan <tariqanwarkhan@users.noreply.github.com>
2016-07-30 03:22:17 -0700

    Added velocity.js library

--- js/app.js
+++ js/app.js
@@ -3,31 +3,22 @@ $(document).ready(function() {
     $("#btnIn").click(
         function() {
             if (opacity === false) {
-                $("#btnCtrl").clearQueue().stop().animate({
+                $("#btnCtrl").velocity({
                     'margin': '0 auto 0'
                 }, 2500);
-                $("#description").animate({
+                $("#description").velocity({
                     'font-size': '1.2em',
                     'height': '1%',
                     'margin': '5% 0 15%',
                     'padding': '10px',
-                    'opacity': '1',
+                    'opacity': '0.9',
                     'text-align': 'left'
-                }, 2500);
+                }, 1000);
                 $("#btnText").html('Hello Ashland JavaScript Meetup!');
                 opacity = true;
             } else {
-                $("#btnCtrl").clearQueue().stop().animate({
-                    'margin': '15% auto 15%'
-                }, 2500);
-                $("#description").animate({
-                    'font-size': '1.2em',
-                    'height': '0',
-                    'margin': '0',
-                    'padding': '0',
-                    'opacity': '0',
-                    'text-align': 'left'
-                }, 1000);
+                $("#btnCtrl").velocity("reverse");
+                $("#description").velocity("reverse");
                 $("#btnText").html('Ashland JavaScript Meetup');
                 opacity = false;
             }
